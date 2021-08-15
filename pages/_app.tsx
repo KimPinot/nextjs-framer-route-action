62243b9b4d022c3129ed37e6c5dd8c37b5760975
PINOT <pinot.kim@kakao.com>
2021-08-16 05:50:08 +0900

    Add animations with framer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { AnimatePresence } from "framer-motion"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <AnimatePresence exitBeforeEnter initial={false}>
+        <Component {...pageProps} />
+      </AnimatePresence>
+  )
 }
 export default MyApp
---
+++ pages/image/[id].tsx
@@ -0,0 +1,52 @@
+import styles from "../../styles/Home.module.css";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { motion } from "framer-motion";
+import { images } from "../../constants/images";
+
+const transition = {
+  duration: 1,
+  ease: [0.43, 0.13, 0.23, 0.96]
+};
+
+const imageVariants = {
+  exit: { y: "50%", opacity: 0, transition },
+  enter: {
+    y: "0%",
+    opacity: 1,
+    transition
+  }
+};
+
+const backVariants = {
+  exit: { x: 100, opacity: 0, transition },
+  enter: { x: 0, opacity: 1, transition: { delay: 1, ...transition } }
+};
+
+const SingleImage = () => {
+  const route = useRouter();
+  const id = String(route.query.id);
+  
+  return (
+      <motion.div className={styles.single} initial="exit" animate="enter" exit="exit">
+        <motion.img
+            variants={imageVariants}
+            src={`https://static1.squarespace.com/static/5b475b2c50a54f54f9b4e1dc/t/${
+                images[parseInt(id, 10)]
+            }.jpg?format=1500w`}
+            alt="The Barbican"
+        />
+        <motion.div className={styles.back} variants={backVariants}>
+          <Link href="/">
+            <a>
+              ← Back
+            </a>
+          </Link>
+        </motion.div>
+      </motion.div>
+  );
+};
+
+export default SingleImage;
